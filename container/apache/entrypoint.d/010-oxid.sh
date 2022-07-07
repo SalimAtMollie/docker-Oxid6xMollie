0bd7629514d32b6e8547f40540e0042b0efbc9fe
Salim Bouziane Rabhi <salimbouzianerabhi@gmail.com>
2022-07-07 14:50:41 +0200

    Removed sed command for base.php

--- container/apache/entrypoint.d/010-oxid.sh
+++ container/apache/entrypoint.d/010-oxid.sh
@@ -8,9 +8,6 @@ then
 	composer config --no-plugins allow-plugins.oxid-esales/oxideshop-unified-namespace-generator true
 	composer config --no-plugins allow-plugins.oxid-esales/oxideshop-composer-plugin true
 	composer require mollie/mollie-oxid
-	ETC="$this->getWebhookUrl()" #Element to change for webhook
-	NE="''https://webhook.site/6ffa5c62-2b0b-4756-83fd-fb030903d376'" #New Element (Random webhookUrl)
-	sed -i "s/$ETC/$NE/g" /var/www/vendor/mollie/mollie-oxid/application/model/request/base.php
 	mv /var/www/config.inc.php /var/www/html/source/config.inc.php
 	chown -R www-data:www-data /var/www/html/source/
 	echo "############################################################################"
