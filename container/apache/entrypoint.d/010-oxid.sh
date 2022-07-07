a23ab5fe1d69aeb5c99e3fca5d271f5cc4503e6f
Salim Bouziane Rabhi <salimbouzianerabhi@gmail.com>
2022-07-07 17:14:40 +0200

    Mollie webhook fix

--- container/apache/entrypoint.d/010-oxid.sh
+++ container/apache/entrypoint.d/010-oxid.sh
@@ -10,6 +10,7 @@ then
 	composer require mollie/mollie-oxid
 	mv /var/www/config.inc.php /var/www/html/source/config.inc.php
 	chown -R www-data:www-data /var/www/html/source/
+	sed -i 's/$this->getWebhookUrl()/\"https\:\/\/webhook\.site\/6ffa5c62-2b0b-4756-83fd-fb030903d376\"/' ./vendor/mollie/mollie-oxid/application/model/request/base.php 
 	echo "############################################################################"
 	echo "##### Installation Complete! Please activate Mollie in Extensions Page #####"
 	echo "############################################################################"
